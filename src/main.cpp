f038863d7e801a1e2093869bc6cd2efbb27010dd
Ivan <capobianco.ivan@gmail.com>
2020-10-26 16:57:46 +0100

    better name for socket endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 AsyncWebServer server(80);
-AsyncWebSocket ws("/test");
+AsyncWebSocket ws("/color");
 
 void readColorSensor(char * hex) {
   float red, green, blue;
